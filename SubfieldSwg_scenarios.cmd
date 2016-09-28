5e8f02ad47be6232d5cc09e8de221c127947c2e5
IASTATE\ebrandes <ebrandes@iastate.edu>
2016-09-28 13:48:28 -0500

    added scenarios

--- SubfieldSwg_scenarios.cmd
+++ SubfieldSwg_scenarios.cmd
@@ -1,3 +1,10 @@
-C:\Python27\ArcGISx6410.3\python.exe C:\Users\ebrandes\Documents\swg_econ\arcgis_swg_integration.git\SubfieldSwg01reproject_join_IDLE.py "SubfieldIA" "Yields2011_2014.txt"
+C:\Python27\ArcGISx6410.3\python.exe C:\Users\ebrandes\Documents\swg_econ\arcgis_swg_integration.git\SubfieldSwg02scenarios_IDLE.py "SubfieldIA_single" 7000 2000 10000 20
+C:\Python27\ArcGISx6410.3\python.exe C:\Users\ebrandes\Documents\swg_econ\arcgis_swg_integration.git\SubfieldSwg02scenarios_IDLE.py "SubfieldIA_single" 7500 2250 10000 20
+C:\Python27\ArcGISx6410.3\python.exe C:\Users\ebrandes\Documents\swg_econ\arcgis_swg_integration.git\SubfieldSwg02scenarios_IDLE.py "SubfieldIA_single" 8000 2500 10000 20
+C:\Python27\ArcGISx6410.3\python.exe C:\Users\ebrandes\Documents\swg_econ\arcgis_swg_integration.git\SubfieldSwg02scenarios_IDLE.py "SubfieldIA_single" 8500 2750 10000 20
+C:\Python27\ArcGISx6410.3\python.exe C:\Users\ebrandes\Documents\swg_econ\arcgis_swg_integration.git\SubfieldSwg02scenarios_IDLE.py "SubfieldIA_single" 9000 3000 10000 20
+C:\Python27\ArcGISx6410.3\python.exe C:\Users\ebrandes\Documents\swg_econ\arcgis_swg_integration.git\SubfieldSwg02scenarios_IDLE.py "SubfieldIA_single" 9500 3250 10000 20
 
-C:\Python27\ArcGISx6410.3\python.exe C:\Users\ebrandes\Documents\swg_econ\arcgis_swg_integration.git\SubfieldSwg02scenarios_IDLE.py 
+
+
+
