71283d2a133a5eeb6cc02e726fb4cd1d1eba1aec
IASTATE\ebrandes <ebrandes@iastate.edu>
2016-10-05 17:02:02 -0500

    could not run script because spatial analyst extension is missing

--- SubfieldSwg05spatial_join_townships.py
+++ SubfieldSwg05spatial_join_townships.py
@@ -1,8 +1,7 @@
 
-import csv # for export into csv file
-import sys # for export into csv file
 import arcpy
-import re # regular expression, to extract the cutoff values out of the scenario names
+from arcpy import env
+from arcpy.sa import *
 
 # set the environment so that output data are being overwritten
 arcpy.env.overwriteOutput=True
@@ -11,17 +10,20 @@ arcpy.env.workspace = "C:\\Users\\ebrandes\\Documents\\DNDC\\switchgrass_integra
 
 # split multipart polygons to single polygons in the feature class "Test_SubfieldIA181_Selection"
 
-#in_feature_class = "Test_SubfieldIA181_Selection"
-#out_feature_class = "test_subfield_single"
-#arcpy.MultipartToSinglepart_management(in_feature_class, out_feature_class)
+in_feature_class = "Test_SubfieldIA181_Selection"
+out_feature_class = "test_subfield_single"
+arcpy.MultipartToSinglepart_management(in_feature_class, out_feature_class)
 
 # make raster layers from the feature classes "test_subfield_single" and "Test_Townships"
 
-in_features = "test_subfield_single"
+in_features = out_feature_class
 value_field = "OBJECTID"
-out_rasterdataset = "test_subfield_raster"
+out_rasterdataset_1 = "test_subfield_raster"
 arcpy.PolygonToRaster_conversion(in_features, value_field, \
-                                 out_rasterdataset, "MAXIMUM_COMBINED_AREA", "", 30)
+                                 out_rasterdataset_1, "MAXIMUM_COMBINED_AREA", "", 30)
+# the OBJECTID from the feature layer turns into the field "value" in the raster. Because not every feature is
+# converted into a raster class because some are too small, the number of OBJECTID in the raster is smaller than
+# the number of OBJECTID in the feature class.
 
 # environment setting: snap raster
 raster = "test_subfield_raster"
@@ -29,9 +31,19 @@ arcpy.env.snapRaster = raster
 
 in_features = "Test_Townships"
 value_field = "politwp_id"
-out_rasterdataset = "test_twp_raster"
+out_rasterdataset_2 = "test_twp_raster"
 arcpy.PolygonToRaster_conversion(in_features, value_field, \
-                                 out_rasterdataset, "MAXIMUM_COMBINED_AREA", "", 30)
+                                 out_rasterdataset_2, "MAXIMUM_COMBINED_AREA", "", 30)
+
+
+# run zonal statistics on the data, using "test_subfield_raster" as the zone raster and
+# "test_twp_raster" as the evaluated raster
+
+in_zone_data = out_rasterdataset_1
+zone_field = "value"
+in_value_raster = out_rasterdataset_2
+out_table = "test_twp_majority"
+arcpy.ZonalStatisticsAsTable(in_zone_data, zone_field, in_value_raster, out_table, "MAJORITY")
 
 
 
