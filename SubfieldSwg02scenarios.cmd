aa41d91b9537546095a23e53f683ada4d2518d6d
IASTATE\ebrandes <ebrandes@iastate.edu>
2016-11-09 09:20:11 -0600

    changed file names

---
+++ SubfieldSwg02scenarios.cmd
@@ -0,0 +1,6 @@
+C:\Python27\ArcGISx6410.3\python.exe C:\Users\ebrandes\Documents\swg_econ\arcgis_swg_integration.git\SubfieldSwg02scenarios.py "SubfieldIA_single" "cut_cg_min_16" "cut_sb_min_16" 10000 20
+
+
+
+
+
