f86c7e8995402ec15142affeaf59a7990ec879fc
IASTATE\ebrandes <ebrandes@iastate.edu>
2016-11-09 15:40:17 -0600

    adapted the code to the new yield scenarios

--- SubfieldSwg03test_areas.py
+++ SubfieldSwg03test_areas.py
@@ -16,10 +16,10 @@ arcpy.env.workspace = "C:\\Users\\ebrandes\\Documents\\DNDC\\switchgrass_integra
 # read through the table and calculate the total area that would be in switchgrass under a certain scenario
 
 # read the fields in a feature class
-#fieldList = arcpy.ListFields(featureClass)
+fieldList = arcpy.ListFields(featureClass)
 # loop through each field in the list and print the name
-#for field in fieldList:
-#    print field.name
+for field in fieldList:
+    print field.name
 
 # Make a list of fields that start with "in_swg*"
 featureClass = sys.argv[1]
@@ -42,7 +42,7 @@ totalArea = 0
 # create a csv file to write the result into
 f = open("C:\\Users\\ebrandes\\Documents\\swg_econ\\swg_areas\\swg_areas_results.csv", 'wb')
 writer = csv.writer(f)
-writer.writerow(('corn_yield_cutoff', 'soy_yield_cutoff', 'size_cutoff',\
+writer.writerow(('yield_scenario', 'yield_years' 'size_cutoff',\
                  'distance_cutoff', 'swg_area', 'swg_area_percent'))
 ######
 
@@ -62,7 +62,8 @@ for field in swgVector:
                 totalArea += row[0]
     totalAreaHa = round(totalArea / 10000, 0)
     print("The total area under scenario " + str(field) + " is " + str(totalAreaHa) + " ha.")
-    values = re.findall(r'\d+', field)
+    values = re.findall(r'\d+', field) # create vector of numeric values in the field
+    values[:0] = [field[7:10]] # add 'min' or '2nd' to describe the scenario
     totalAreaPercent = round((totalAreaHa *100/totalCornSoyHa),1)
     writer.writerow((str(values[0]), str(values[1]), str(values[2]), str(values[3]), str(totalAreaHa), str(totalAreaPercent)))
 f.close()
