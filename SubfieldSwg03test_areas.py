879a87752933111f14cede06a5b489b9ab54f723
IASTATE\ebrandes <ebrandes@iastate.edu>
2016-11-09 16:58:09 -0600

    updated further - ran successfully

--- SubfieldSwg03test_areas.py
+++ SubfieldSwg03test_areas.py
@@ -15,11 +15,6 @@ arcpy.env.workspace = "C:\\Users\\ebrandes\\Documents\\DNDC\\switchgrass_integra
 
 # read through the table and calculate the total area that would be in switchgrass under a certain scenario
 
-# read the fields in a feature class
-fieldList = arcpy.ListFields(featureClass)
-# loop through each field in the list and print the name
-for field in fieldList:
-    print field.name
 
 # Make a list of fields that start with "in_swg*"
 featureClass = sys.argv[1]
@@ -37,6 +32,7 @@ print(swgVector)
 
 # create variables
 area_field = "Shape_Area"
+fips_field = "fips"
 totalArea = 0
 
 # create a csv file to write the result into
@@ -48,10 +44,12 @@ writer.writerow(('yield_scenario', 'yield_years' 'size_cutoff',\
 
 # calculate total area in corn/soybean first to be able to calculate percentages below
 totalCornSoy = 0
-with arcpy.da.SearchCursor(featureClass, (area_field,)) as cursor:
+with arcpy.da.SearchCursor(featureClass, (fips_field, area_field,)) as cursor:
     for row in cursor:
-        totalCornSoy += row[0]
+        if row[0] is not None:
+            totalCornSoy += row[1]
 totalCornSoyHa = round(totalCornSoy / 10000, 0)
+print("The total area in corn and soybean is " + str(totalCornSoyHa) + " ha.")
 
 # loop through the vector of column names to get a total ha value for each scenario
 for field in swgVector:
@@ -63,6 +61,7 @@ for field in swgVector:
     totalAreaHa = round(totalArea / 10000, 0)
     print("The total area under scenario " + str(field) + " is " + str(totalAreaHa) + " ha.")
     values = re.findall(r'\d+', field) # create vector of numeric values in the field
+    values = values[-3:] # get rid of the first '2' if exists
     values[:0] = [field[7:10]] # add 'min' or '2nd' to describe the scenario
     totalAreaPercent = round((totalAreaHa *100/totalCornSoyHa),1)
     writer.writerow((str(values[0]), str(values[1]), str(values[2]), str(values[3]), str(totalAreaHa), str(totalAreaPercent)))
